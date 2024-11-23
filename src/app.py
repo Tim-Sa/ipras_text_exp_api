7ffdeb323cbfb517b62e34fc245e32e6b2de9fb1
Tim-Sa <salnikovtimofeyof@gmail.com>
2024-11-23 18:00:03 +0300

    validation and add user info

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 # main.py
+from typing import List
 from fastapi import FastAPI, HTTPException
-from database import PoolProvider, write_answer
+import database as db
 from model import Answer
 
 app = FastAPI()
@@ -8,30 +9,94 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    await PoolProvider.init_pool()
+    await db.PoolProvider.init_pool()
 
 
 @app.on_event("shutdown")
 async def shutdown():
-    await PoolProvider.close_pool()
+    await db.PoolProvider.close_pool()
 
 
 @app.post("/answers/", response_description="Добавить новый ответ")
 async def create_answer(answer: Answer):
     """
-    Эндпоинт для добавления нового ответа в базу данных.
+    Добавление нового ответа в базу данных.
     :param answer: Ответ, который нужно сохранить. 
                    Поля: user_id, text_id, difficult, interest.
     :return: Сообщение об успешном добавлении.
     """
     try:
-        answer_id = await write_answer(PoolProvider.pool, answer)
+        answer_id = await db.write_answer(db.PoolProvider.pool, answer)
         return {"answer_id": answer_id, "message": "Ответ успешно добавлен!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@app.get("/answers/user/{user_id}", response_model=List[Answer])
+async def get_answers_by_user(user_id: int):
+    """
+    Получить все ответы пользователя по user_id.
+    :param user_id: ID пользователя.
+    :return: Список ответов пользователя.
+
+    """
+    if user_id <= 0:
+        raise HTTPException(status_code=400, detail="user_id должен быть положительным")
+
+    try:
+        answers = await db.get_answers_by_user_id(db.PoolProvider.pool, user_id)
+        return answers
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/answers/text/{text_id}", response_model=List[Answer])
+async def get_answers_by_text(text_id: int):
+    """
+    Получить все ответы пользователя по text_id.
+    :param text_id: ID текста.
+    :return: Список ответов по указанному text_id.
+    """
+
+    if text_id <= 0:
+        raise HTTPException(status_code=400, detail="text_id должен быть положительным")
+
+    try:
+        answers = await db.get_answers_by_text_id(db.PoolProvider.pool, text_id)
+        return answers
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/answers/", response_description="Добавить новый ответ")
+async def create_answer(answer: Answer):
+    """
+    Добавление нового ответа в базу данных.
+    :param answer: Ответ, который нужно сохранить. 
+                   Поля: user_id, text_id, difficult, interest.
+    :return: {"answer_id": answer_id, "message": "Ответ успешно добавлен!"}
+    """
+    try:
+        answer_id = await db.write_answer(db.PoolProvider.pool, answer)
+        return {"answer_id": answer_id, "message": "Ответ успешно добавлен!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+@app.post("/users/", response_description="Добавить нового пользователя")
+async def add_user():
+    """
+    Добавление нового пользователя в базу данных.
+    :return: {"user_id": user_id, "message": "Пользователь успешно добавлен!"}
+    """
+    try:
+        user_id = await db.create_user(db.PoolProvider.pool)
+        return {"user_id": user_id, "message": "Пользователь успешно добавлен!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 if __name__ == "__main__":
-    # Для запуска приложения с uvicorn
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 
 import asyncpg
@@ -44,18 +45,98 @@ async def create_pool():
     return await asyncpg.create_pool(dsn)
 
 
-async def write_answer(pool, answer: Answer) -> int:
+async def is_text_exists(pool: any, text_id: int) -> bool:
     async with pool.acquire() as connection:
-            query = """
-            INSERT INTO answer (user_id, text_id, difficult, interest)
-            VALUES ($1, $2, $3, $4)
-            RETURNING answer_id;
-            """
-            answer_id = await connection.fetchval(query, 
-                                                  answer.user_id, 
-                                                  answer.text_id, 
-                                                  answer.difficult, 
-                                                  answer.interest)
+        text_exists = await connection.fetchval(
+            'SELECT EXISTS(SELECT 1 FROM public.text WHERE text_id = $1)', text_id
+        )
+        return text_exists
 
-            return answer_id
-    
+
+async def is_user_exists(pool: any, user_id: int) -> bool:
+    async with pool.acquire() as connection:
+        user_exists = await connection.fetchval(
+            'SELECT EXISTS(SELECT 1 FROM public.user WHERE user_id = $1)', user_id
+        )
+        return user_exists
+
+
+async def write_answer(pool: any, answer: Answer) -> int:
+    async with pool.acquire() as connection:
+        query_insert = """
+        INSERT INTO public.answer (user_id, text_id, difficult, interest)
+        VALUES ($1, $2, $3, $4)
+        RETURNING answer_id;
+        """
+        
+        query_check_existence = """
+        SELECT COUNT(*)
+        FROM public.answer
+        WHERE user_id = $1 AND text_id = $2;
+        """
+
+        is_exists = await is_text_exists(pool, answer.text_id)
+        if not is_exists:
+            raise ValueError("Text with provided id doesn't exists")
+
+        is_exists = await is_user_exists(pool, answer.user_id)
+        if not is_exists:
+            raise ValueError("User with provided id doesn't exists")
+
+        count = await connection.fetchval(query_check_existence, answer.user_id, answer.text_id)
+        if count > 0:
+            raise ValueError(f"Answer already exists for user_id {answer.user_id} and text_id {answer.text_id}")
+
+        # If no existing answer, insert the new one
+        answer_id = await connection.fetchval(query_insert, 
+                                              answer.user_id, 
+                                              answer.text_id, 
+                                              answer.difficult, 
+                                              answer.interest)
+
+        return answer_id
+
+
+async def get_answers_by_text_id(pool: any, text_id: int) -> List[Answer]:
+    async with pool.acquire() as connection:
+        query = """
+        SELECT user_id, text_id, difficult, interest
+        FROM public.answer
+        WHERE text_id = $1;
+        """
+
+        is_exists = await is_text_exists(pool, text_id)
+        if not is_exists:
+            raise ValueError("Text with provided id doesn't exists")
+
+        rows = await connection.fetch(query, text_id)
+        answers = [Answer(**row) for row in rows]
+
+        return answers
+
+
+async def get_answers_by_user_id(pool: any, user_id: int) -> List[Answer]:
+    async with pool.acquire() as connection:
+        query = """
+        SELECT user_id, text_id, difficult, interest
+        FROM public.answer
+        WHERE user_id = $1;
+        """
+
+        is_exists = await is_user_exists(pool, user_id)
+        if not is_exists:
+            raise ValueError("User with provided id doesn't exists")
+
+        rows = await connection.fetch(query, user_id)
+        answers = [Answer(**row) for row in rows]
+
+        return answers
+
+
+async def create_user(pool: any) -> int:
+    async with pool.acquire() as connection:
+        query_insert_user = """
+        INSERT INTO public.user DEFAULT VALUES RETURNING user_id;
+        """
+        user_id = await connection.fetchval(query_insert_user)
+        return user_id
